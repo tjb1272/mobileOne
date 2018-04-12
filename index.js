5e448a21cc020ae7b88f66201fc2a9ed496c839b
tjb1272 <tjb1272@gmail.com>
2018-04-11 18:24:37 -0700

    Update MobileOne add automation

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const app = express();
+const session = require('cookie-session');
+const handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
 const port = process.env.PORT || 3000;
+const path = require('path');
+
+
+//Engines
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Express Session
+// app.use(session({
+//     secret: 'secret',
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: { secure: true }
+// }));
 
 app.listen(3000, () => {
     console.log('server running')
